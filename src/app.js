8c9006b2aabd12d568af96a501fcc04c9d108b44
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-20 22:33:36 +0000

    not fixed

--- src/app.js
+++ src/app.js
@@ -7,10 +7,56 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let generateExcuse = () => {
-    let symbol = ["♦", "♥", "♠", "♣"];
-    let symbolIndex = Math.floor(Math.random() * symbol.length);
-
-    return symbol[symbolIndex];
-  };
+  document.querySelector('.card')classList.add('heart'); 
+  setTimeout(() => {
+    document.querySelector('.card')classList.remove('heart'); 
+    document.querySelector('.card')classList.add('spade'); 
+  }, 1000);
 };
+let generateRandomNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbolIndex = Math.floor(Math.random() * symbol.length);
+  let numberIndex = Math.floor(Math.random() * number.length);
+
+
+  return symbol[symbolIndex];
+};
+
+let generateRandomSymbol = () => {
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbolIndex = Math.floor(Math.random() * symbol.length);
+  let numberIndex = Math.floor(Math.random() * number.length);
+
+
+  return symbol[symbolIndex];
+};
+let symbolIndex = Math.floor(Math.random() * symbol.length);
